d5bdc58e567c02e08c3ce944b1870c8fab839e2b
Jay Chawda <jchawda@phonepe.com>
2020-02-11 15:09:12 +0530

    Some cleanup

--- request.lua
+++ request.lua
@@ -1,20 +1,7 @@
 
-function base64enc(data)
-	local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' -- You will need this for encoding/decoding
-    return ((data:gsub('.', function(x) 
-        local r,b='',x:byte()
-        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
-        return r;
-    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
-        if (#x < 6) then return '' end
-        local c=0
-        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
-        return b:sub(c+1,c+1)
-    end)..({ '', '==', '=' })[#data%3+1])
-end
 request = function() 
 
-	local chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^*()_+-{}|[]~'
+	local chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 	local length = 260
 	local randomString = ''
 
@@ -30,10 +17,10 @@ request = function()
 	end
 
 
-	local payload = ("payload=" .. randomString)
+	local payload = randomString
 
 	
-	local hmac = base64enc(randomString)
+	local hmac = string.reverse(payload)
 
 	wrk.method = "POST"
 	wrk.body   = ("payload="..payload.."&hmac="..hmac)
